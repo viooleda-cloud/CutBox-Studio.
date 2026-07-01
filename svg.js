7f88ceba8249e99701bdc0f4383b64aa3cc9c2dd
viooleda-cloud <vioole.d.a@gmail.com>
2026-07-01 18:57:55 -0300

    Update svg.js

--- svg.js
+++ svg.js
@@ -1,1 +1,23 @@
+function iniciarSVG(){
 
+return `
+<svg xmlns="http://www.w3.org/2000/svg"
+width="210mm"
+height="297mm"
+viewBox="0 0 210 297">
+
+<g fill="none"
+stroke="black"
+stroke-width="0.30">
+
+`;
+}
+
+function cerrarSVG(){
+
+return `
+</g>
+</svg>
+`;
+
+}
