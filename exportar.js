3036b73428e283befe9c62fb1a75fa3df1644404
viooleda-cloud <vioole.d.a@gmail.com>
2026-07-01 18:58:53 -0300

    Update exportar.js

--- exportar.js
+++ exportar.js
@@ -1,1 +1,10 @@
+function mostrarSVG(svg, nombreArchivo){
+    document.getElementById("resultado").innerHTML =
+        svg +
+        "<br><br><a class='descarga' download='" +
+        nombreArchivo +
+        ".svg' href='data:image/svg+xml;charset=utf-8," +
+        encodeURIComponent(svg) +
+        "'>📥 Descargar SVG</a>";
 
+}
