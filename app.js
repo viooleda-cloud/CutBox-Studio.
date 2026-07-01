858950a48ababd8d9b178deee92bd218d3be85e2
viooleda-cloud <vioole.d.a@gmail.com>
2026-07-01 19:01:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
+function generarPlantilla(){
+    const modelo=document.getElementById("modelo").value;
 
+    const ancho=Number(document.getElementById("ancho").value);
+    const alto=Number(document.getElementById("alto").value);
+    const prof=Number(document.getElementById("prof").value);
+
+    let svg="";
+
+    switch(modelo){
+
+        case "caja-simple":
+            svg=cajaSimple(ancho,alto,prof);
+            mostrarSVG(svg,"caja_simple");
+            break;
+
+        case "caja-tapa":
+            alert("Próximamente");
+            break;
+
+        case "sobre":
+            svg=sobreProfesional(ancho,alto);
+            mostrarSVG(svg,"sobre");
+            break;
+
+        case "etiqueta-rect":
+            alert("Próximamente");
+            break;
+
+        case "etiqueta-redonda":
+            alert("Próximamente");
+            break;
+
+        default:
+            alert("Modelo no encontrado");
+
+    }
+
+}
