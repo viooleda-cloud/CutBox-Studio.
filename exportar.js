38da8079f953a2d098b1c7ef046b2b23fc805ae3
viooleda-cloud <vioole.d.a@gmail.com>
2026-07-01 20:30:39 -0300

    Update exportar.js

--- exportar.js
+++ exportar.js
@@ -1,10 +1,11 @@
-function mostrarSVG(svg, nombreArchivo){
-    document.getElementById("resultado").innerHTML =
-        svg +
-        "<br><br><a class='descarga' download='" +
-        nombreArchivo +
-        ".svg' href='data:image/svg+xml;charset=utf-8," +
-        encodeURIComponent(svg) +
-        "'>📥 Descargar SVG</a>";
+function mostrarSVG(data,nombreArchivo){
+  let svg = data.svg || data;
+  let totalW = data.totalW || 0;
+  let totalH = data.totalH || 0;
+  let ok = entraEnA4(totalW,totalH);
 
+  document.getElementById("resultado").innerHTML =
+    svg +
+    `<br><a class="descarga" download="${nombreArchivo}.svg" href="data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}">📥 Descargar SVG</a>` +
+    `<div class="info ${ok?'ok':'warn'}">${ok ? 'Entra en A4' : 'Atención: puede no entrar en A4'} · Tamaño aproximado: ${n(totalW)} x ${n(totalH)} mm</div>`;
 }
